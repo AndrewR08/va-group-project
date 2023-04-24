9ed4ed84413daed8725150b4c1b54450aaa4e368
AndrewR08 <reevesat@appstate.edu>
2023-04-24 14:00:17 -0400

    added initial files

---
+++ control.js
@@ -0,0 +1,23 @@
+new (class Control {
+    #viewA
+    #viewB
+
+    constructor() {
+        const root = d3.select('body').append('div')
+            .style('height', '10f00')
+            .style('width', '1000')
+
+        this.#viewA = new ViewA(this, root, 'A', 0, 0, '50%', '100%', 'red')
+        this.#viewB = new ViewB(this, root, 'B', '50%', 0, '50%', '100%', 'green')
+    }
+
+    showViewName(viewName){
+        // console.log(`clicked! ${viewName}`)
+        this.#viewA.showLabel(viewName, 'red')
+        this.#viewB.showLabel(viewName, 'green')
+    }
+
+    Test(str) {
+        console.log(str)
+    }
+})()
---
+++ viewB.js
@@ -0,0 +1,50 @@
+class ViewB {
+    constructor(con, root, name, x, y, w, h, color) {
+        this.con = con
+        this.name = name
+
+        const div = root.append('div')
+            .style('position', 'absolute')
+            .style('width', `${w}`)
+            .style('height', `${h}`)
+            .style('left', `${x}`)
+            .style('top', `${y}`)
+
+        const svg = div.append('svg')
+            .attr('width', '100%')
+            .attr('height', '100%')
+
+        svg.append('rect')
+            .attr('id', `${name}-rect`)
+            .attr('width', '100%')
+            .attr('height', '100%')
+            .on('click', () => {
+                this.con.showViewName(name)
+            })
+            .attr('fill', color)
+        
+        svg.append('text')
+            .attr('x', '50%')
+            .attr('y', '50%')
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'middle')
+            .attr('fill', 'white')
+            .attr('font-size', '36px')
+            .text(name)
+
+        svg.append('text')
+            .attr('id', `${name}-label`)
+            .attr('x', '50%')
+            .attr('y', '60%')
+            .attr('text-anchor', 'middle')
+            .attr('fill', 'black')
+            .attr('font-size', '24px')
+
+        con.Test(`View ${name} is connected.`)
+    }
+
+    showLabel(viewName, tcolor) {
+        const label = d3.select(`#${this.name}-label`)
+        label.text(`View ${viewName} Selected`)
+    }
+}
