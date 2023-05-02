6b4e6f55093f89bcb028b45b65789498b238995c
Alexander Clarke <alex-p-clarke@hotmail.com>
2023-05-02 14:51:55 -0400

    modified some of the word cloud styles

--- viewB.js
+++ viewB.js
@@ -5,7 +5,6 @@ class ViewB {
         this.con = con
         this.name = name
 
-        console.log(con)
         d3.csv('correlations.csv').then(data =>{
             console.log(data)
 
@@ -19,46 +18,79 @@ class ViewB {
                 .html('Variance of Genre');
 
             const svg = div.append('svg')
-                .attr("viewBox", "0 0 1000 1000")
-                .style('margin-top', "max(0px, calc(((100vh - 35px - 8px - 40px) - (50vw - 6px)) / 2))")
-                .style('margin-bottom', "max(0px, calc(((100vh - 35px - 8px - 40px) - (50vw - 6px)) / 2))")
-                .style('height', 'min(calc(100vh - 8px - 35px - 40px), calc(50vw - 6px))')
-                .style('width', 'min(calc(100vh - 8px - 35px), calc(50vw - 6px))');
+                .attr("viewBox", "-500 -500 1000 1000")
+                .style('margin-top', "max(0px, calc(((100vh - 40px - 8px - 40px) - (50vw - 6px)) / 2))")
+                .style('margin-bottom', "max(0px, calc(((100vh - 40px - 8px - 40px) - (50vw - 6px)) / 2))")
+                .style('height', 'min(calc(100vh - 8px - 40px - 40px), calc(50vw - 6px))')
+                .style('width', 'min(calc(100vh - 8px - 40px), calc(50vw - 6px))');
 
             div.append('input')
                 .attr('id', 'clear-button')
                 .attr('type', 'button')
                 .attr('value', 'CLEAR SELECTION')
                 .on('click', () => {
-                    // TODO: Clear the selected genres.
-                    console.log('clear')
                     const matches = document.querySelectorAll('[fill*="red"]')
                     matches.forEach( x => d3.select(x).attr("fill", null) )
                     matches.forEach( x => selected_genres.splice(selected_genres.indexOf(d3.select(x).text()), 1))
                     con.updateSelected(selected_genres)
                 });
 
+            const word_size_scale = d3.scaleLinear()
+                .domain(d3.extent(data, (d) => d.std_dev))
+                .range([15, 50])
+
             const layout = d3.layout.cloud()
                 .size([1000, 1000])
                 .words(data)
-                .padding(5)
-                .rotate(()=>Math.floor(Math.random()*2)*270)
-                .fontSize(d=>(d.std_dev*50))
+                .padding(10)
+                .rotate(0)
+                .fontSize((d) => word_size_scale(d.std_dev))
                 .on("end", draw)
-                //.style("cursor", "default")
 
             layout.start()
 
             const tooltip = d3.select("body")
-                    .append("div")
-                    .style("position", "absolute")
-                    .style("visibility", "hidden")
-                    .style("background", "#000")
+                .append("div")
+                .style("position", "absolute")
+                .style("visibility", "hidden")
+                .style("background-color", "rgba(0, 0, 0, 0.8)")
+                .style("pointer-events", "none")
+                .style("transform", "translate(-50%, 15px)")
+                .style('padding', '5px')
+                .style("text-align", "center")
+            
+            tooltip.append('div')
+                .attr('id', 'genre-pop')
+            
+            tooltip.append('div')
+                .attr('id', 'genre-std')
+
+            function updateToolTip(d) {
+                let sd_var = d3.select(this).data()[0]['std_dev']
+                let pop_var = d3.select(this).data()[0]['avg_popularity']
+                sd_var = parseFloat(sd_var).toFixed(2)
+                pop_var = parseFloat(pop_var).toFixed(2)
+
+                d3.select(this).transition()
+                    .duration('50')
+                    .attr('opacity', '.85')
+
+                tooltip.style("visibility", "visible")
+                    .style("left", d.x + "px")
+                    .style("top", d.y + "px")
+                    .style("color", "white")
+
+                d3.select('#genre-std')
+                    .text(`Std Dev: ${sd_var}`) 
+                
+                d3.select('#genre-pop')
+                    .text(`Popularity: ${pop_var}`)
+
+            }
 
             function draw(words) {
                 svg
                     .append("g")
-                    .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
                     .selectAll("text")
                     .data(words)
                     .enter()
@@ -68,54 +100,32 @@ class ViewB {
                     .style("font-family", "Impact")
                     .attr("text-anchor", "middle")
                     .attr("transform", function (d) { return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"})
-                    .attr('opacity', (d) => (d['avg_popularity']))
-                    .style("cursor", "default")
+                    .attr('opacity', (d) => ((d['avg_popularity'] * 0.8) + 0.2))
+                    .style("text-transform", "uppercase")
+                    .style("cursor", "pointer")
 
                     .on('click', function(d) {
-                        //d3.select(this).style("cursor", "default")
-
                         const genre_var = d3.select(this).text()
                         const pop_var = d3.select(this).data()[0]['avg_popularity']
                         const color = d3.select(this).attr('fill')
 
-                        if (color==null) {
+                        if (color == null) {
                             d3.select(this).attr("fill", "red")
-                            //d3.select(this).attr("opacity", "1")
                             selected_genres.push(genre_var)
-                        }
-                        else {
+                        } else {
                             d3.select(this).attr("fill", null)
-                            d3.select(this).attr("opacity", pop_var)
+                            d3.select(this).attr("opacity", (pop_var * 0.8) + 0.2)
                             selected_genres.splice(selected_genres.indexOf(genre_var), 1)
                         }
                         con.updateSelected(selected_genres)
                     })
 
-                    //display std_dev and popularity values for hovered genre
-                    .on('mouseover', function (d, i) {
-                        //d3.select(this).style("cursor", "default")
 
-                        var sd_var = d3.select(this).data()[0]['std_dev']
-                        var pop_var = d3.select(this).data()[0]['avg_popularity']
-                        sd_var = parseFloat(sd_var).toFixed(2)
-                        pop_var = parseFloat(pop_var).toFixed(2)
-                        //console.log(pop_var)
-
-                        d3.select(this).transition()
-                            .duration('50')
-                            .attr('opacity', '.85')
-
-                        tooltip.text(`Std Dev: ${sd_var}   Popularity: ${pop_var}`)
-                                .style("visibility", "visible")
-                                .style("left", d.x + "px")     
-                                .style("top", d.y + "px")
-                                .style("color", "white")
-
-                    })
                     
-                    .on("mousemove", function() {
-                        //d3.select(this).style("cursor", "default")
-                    })                    
+                    //display std_dev and popularity values for hovered genre
+                    .on('mouseover', updateToolTip)
+                    
+                    .on("mousemove", updateToolTip)                    
 
                     .on('mouseout', function (d, i) {
                         //d3.select(this).style("cursor", "default")
@@ -125,7 +135,7 @@ class ViewB {
                         
                         d3.select(this).transition()
                             .duration('50')
-                            .attr('opacity', pop_var)
+                            .attr('opacity', (pop_var * 0.8) + 0.2)
 
                         tooltip.style("visibility", "hidden")
                     })
